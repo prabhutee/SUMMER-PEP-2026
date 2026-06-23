a341a6d77864c68979d081d671ec527578ea0142
Prabhutee Neb <nebprabhutee@gmail.com>
2026-06-23 09:25:31 +0530

    Basics

---
+++ Basics.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int age;
+    // cout<<age<<endl;
+    //int->4 bytes
+    //long long ->8 bytes
+    //float ->4 bytes
+    //double ->8 bytes
+    //char ->1 byte
+    //bool ->1 byte
+
+
+    // char name='A';
+    // bool answer=true;
+
+    //CONDITIONAL STATEMENTS
+    // if()
+    // {
+
+    // }
+    // else
+    // {
+
+    // }
+
+    // int age;
+    // int number;
+    // cout<<"Enter your age: ";
+    // cin>>age;
+    // cout<<"Enter your number: ";
+    // cin>>number;
+    // if(age>18 && number>10)
+    // {
+    //     cout<<"Test will be conducted"<<endl;
+    // }
+    // else
+    // {
+    //     cout<<"Test will not be conducted"<<endl;   
+    // }
+
+    // char grade='C';
+    // switch(grade)
+    // {
+    //     case 'A':
+    //         cout<<"Excellent"<<endl;
+    
+    //     case 'B':
+    //         cout<<"Good"<<endl;
+            
+    //     case 'C':
+    //         cout<<"Average"<<endl;
+            
+    //     default:
+    //         cout<<"Invalid grade"<<endl;
+    // }
+
+    //  return 0;
+
+    //  int X=10, Y =20;
+    // (X>Y)  ? cout<<"X" <<endl : cout<<"Y" <<endl;
+
+
+    for(int i=0; i<5; i++)
+    {
+        cout<<i<<endl;
+    }
+
+    
+}
