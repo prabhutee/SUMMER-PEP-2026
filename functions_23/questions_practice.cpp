e5b607d541d63b0afae0790d55bfd1a241070f84
Prabhutee Neb <nebprabhutee@gmail.com>
2026-06-28 16:55:58 +0530

    26/6/26array_ques

--- functions_23/questions_practice.cpp
+++ functions_23/questions_practice.cpp
@@ -1,3 +1,5 @@
+//LS::
+//BS:
 #include<iostream>
 using namespace std;
 
@@ -24,6 +26,30 @@ int main()
         {
             cout<< key/2;
         }
+
+        //largest
+        //second largest
+        //count max consecutive ones
+        //max consecutive bits
+        //count zeroes
+        //move zeroes to end
+
+        //25/6/26:
+        //lc 167
+        //lc 121
+        //three triplet sum
+        //lc88merge sorted array 
+        //sort colors
+        
+
+        //26/6/26:
+        //pair sum in rotated sorted array
+        //find the first and last position of an element in sorted array.
+        //floor
+        //ceil
+        
+
+
 
     }
 }
