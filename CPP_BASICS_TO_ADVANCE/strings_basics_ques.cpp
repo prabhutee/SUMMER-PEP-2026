c5ff3bdf2596f0c751f9124cc56f1883080e50b8
Prabhutee Neb <nebprabhutee@gmail.com>
2026-07-06 10:22:57 +0530

    6/7/21_linkedlist

--- CPP_BASICS_TO_ADVANCE/strings_basics_ques.cpp
+++ CPP_BASICS_TO_ADVANCE/strings_basics_ques.cpp
@@ -31,3 +31,42 @@ cout<<str<<endl;
 
 
 //string duplicates removal
+
+//2/7/26:
+//VALID ANAGRAM
+//VALID PALINDROME
+//VALID PALINDROME II
+//IS SUBSEQUENCE
+//FIRST REPEATED CHARACTER
+//LC 1456
+//LC 209
+//TEST PRACTICE:
+//bs on answers
+//rotated sorted array
+//two pointers
+// 977
+// 167
+// 15
+// 18
+// 1498
+
+// PREFIX SUM:
+// 724
+// 238
+//SLIDING WINDOW:FIX DYNAMIC
+// 643
+// 1456
+// 1343
+
+// DYNAMIC:
+// 1004
+
+
+//diff pattern:
+// longest substring with longest unique characters
+// 904
+
+//3/7/21:missed
+//insertion at beginning , end middle in linked list
+//print list
+
