7f598fba410b1797c0871f3efcf6c1b2604b3813
Prabhutee Neb <nebprabhutee@gmail.com>
2026-07-08 09:24:21 +0530

    8/7/21_linkedlistques

--- CPP_BASICS_TO_ADVANCE/CircularLinkedlist.cpp
+++ CPP_BASICS_TO_ADVANCE/CircularLinkedlist.cpp
@@ -1,3 +1,4 @@
+//CIRCULAR LINKED LIST:
 #include<iostream>
 using namespace std;
 class Node
@@ -11,4 +12,127 @@ class Node
         data=val;
         next=NULL;
     }
+}
+void insertAtEnd(Node* &head,int val)
+{
+    Node* node=new Node(val);
+    if(head==NULL)
+    {
+        head=node;
+        node->next=head;
+        return;
+    }
+    Node* temp=head;
+    while(temp->next!=head)
+    {
+        temp=temp->next;
+    }
+    temp->next=node;
+    node->next=head;
+}
+void insertAtBeginning(Node* &head,int val)
+{
+    Node* node=new Node(val);
+    if(head==NULL)
+    {
+        head=node;
+        node->next=head;
+        return;
+    }
+    Node* temp=head;
+    while(temp->next!=head)
+    {
+        temp=temp->next;
+    }
+    temp->next=node;
+    node->next=head;
+    head=node;
+}
+void deleteAtEnd(Node* &head)
+{
+    if(head==NULL)
+    {
+        return;
+    }
+    if(head->next==head)
+    {
+        delete head;
+        head=NULL;
+        return;
+    }
+    Node* temp=head;
+    while(temp->next->next!=head)
+    {
+        temp=temp->next;
+    }
+    delete temp->next;
+    temp->next=head;
+}
+
+void deleteAtBeginning(Node* &head)
+{
+    if(head==NULL)
+    {
+        return;
+    }
+    if(head->next==head)
+    {
+        delete head;
+        head=NULL;
+        return;
+    }
+    Node* temp=head;
+    while(temp->next!=head)
+    {
+        temp=temp->next;
+    }
+    Node* todelete=head;
+    temp->next=head->next;
+    head=head->next;
+    delete todelete;
+}
+void deleteAtPosition(Node* &head,int pos)
+{
+    if(head==NULL)
+    {
+        return;
+    }
+    if(pos==1)
+    {
+        deleteAtBeginning(head);
+        return;
+    }
+    Node* temp=head;
+    for(int i=1;i<pos-1 && temp->next!=head;i++)
+    {
+        temp=temp->next;
+    }
+    if(temp->next==head)
+    {
+        return;
+    }
+    Node* todelete=temp->next;
+    temp->next=todelete->next;
+    delete todelete;
+}
+void display(Node* head)
+{
+    if(head==NULL)
+    {
+        return;
+    }
+    Node* temp=head;
+    do
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }while(temp!=head);
+    cout<<endl;
+}
+
+int main()
+{
+    Node* head=NULL;
+    insertAtEnd(head, 1);
+    insertAtBeginning(head, 5);
 }
---
+++ CPP_BASICS_TO_ADVANCE/LinkedList_ques.cpp
@@ -0,0 +1,7 @@
+//Linkedlist ques:
+//of leetcode and gfg
+
+//FLOYD LOOP DETECTION:
+//detect if loop  exists
+//find the starting node of loop
+//remove the loop
