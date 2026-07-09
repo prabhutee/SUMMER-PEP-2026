93ac7d1446dbc22aaa0b5c6d554072c7bc67f15c
Prabhutee Neb <nebprabhutee@gmail.com>
2026-07-09 11:51:33 +0530

    9/7/21_LLqueshard

--- CPP_BASICS_TO_ADVANCE/LinkedList_ques.cpp
+++ CPP_BASICS_TO_ADVANCE/LinkedList_ques.cpp
@@ -5,3 +5,9 @@
 //detect if loop  exists
 //find the starting node of loop
 //remove the loop
+
+
+//delete the nth node from end of linked list
+//partition list(gfg doubt)
+//reverse k nodes in LL(25)
+//LRU CACHE
